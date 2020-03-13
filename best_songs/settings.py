ac334fb3ea23129ff88ba9d0bfb71cc8f2ba6a2d
johnchinaski <tatophoenix666@gmail.com>
2020-03-13 16:22:31 -0300

    [ADD] - Configure docker and start Django app get_top_ten

--- best_songs/settings.py
+++ best_songs/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'best_songs.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
---
+++ get_top_ten/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GetTopTenConfig(AppConfig):
+    name = 'get_top_ten'
